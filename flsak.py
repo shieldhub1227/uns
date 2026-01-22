d5379e6918f0da61da48db18cab231a56e71077b
shieldhub1227 <shieldhub1227@gmail.com>
2026-01-23 00:37:35 +0900

    Update flsak.py

--- flsak.py
+++ flsak.py
@@ -71,4 +71,5 @@ def cleanup():
 threading.Thread(target=cleanup, daemon=True).start()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
